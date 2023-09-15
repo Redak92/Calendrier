b54ca19f9a43acaebc730bb76c67bc0c97f54c91
Redak92 <bastienguibert98@gmail.com>
2023-09-16 01:07:02 +0200

    p

--- Suite.py
+++ Suite.py
@@ -67,12 +67,14 @@ def ex5part2():
 
 
 # Exercice 6
-
+def print_matrix(matrix):
+    for i in matrix:
+        print(i)
 def ex6():
     with open("input.txt", 'r') as fichier:
         tab = fichier.readlines()
 
-    lights = [(i, x, -1) for i in range(0, 10) for x in range(0, 10)]
+    lights = [[0 for i in range(0, 1000)] for x in range(0, 1000)]
     taille = len(tab)
 
     for i in range(taille):
@@ -85,17 +87,89 @@ def ex6():
             first_data = tab[i][1].split(',')
             second_data = tab[i][t - 1].split(',')
 
-            for ab_x in range(int(first_data[0]), int(first_data[1])):
-                for ab_y in range(int(second_data[0]),int(second_data[1])):
-                    lights[ab_x][ab_y] *= -1
+            for ab_y in range(int(first_data[1]),int(second_data[1]) + 1):
+                for ab_x in range(int(first_data[0]), int(second_data[0]) + 1):
+                    lights[ab_x][ab_y] = 1 if lights[ab_x][ab_y] == 0 else 0
+
+
 
         else:
 
             t = 5
             first_data = tab[i][2].split(',')
             second_data = tab[i][t - 1].split(',')
-    print(lights)
-    print(tab)
+
+            if tab[i][1] == "on":
+                change = 1
+            else:
+                change = 0
 
 
-ex6()
+
+            for ab_y in range(int(first_data[1]),int(second_data[1]) + 1):
+                for ab_x in range(int(first_data[0]), int(second_data[0]) + 1):
+                    lights[ab_x][ab_y] = change
+    return count_lights(lights)
+def count_lights(lights):
+    cpt = 0
+    for i in lights:
+        for x in i:
+            if x == 1:
+                cpt += 1
+    return cpt
+
+
+def ex6part2():
+    with open("input.txt", 'r') as fichier:
+        tab = fichier.readlines()
+
+    lights = [[0 for i in range(0, 1000)] for x in range(0, 1000)]
+    taille = len(tab)
+
+    for i in range(taille):
+        tab[i] = tab[i][:-1]
+
+    for i in range(taille):
+        tab[i] = tab[i].split(" ")
+        if tab[i][0] == "toggle":
+            t = 4
+            first_data = tab[i][1].split(',')
+            second_data = tab[i][t - 1].split(',')
+
+            for ab_y in range(int(first_data[1]),int(second_data[1]) + 1):
+                for ab_x in range(int(first_data[0]), int(second_data[0]) + 1):
+                    lights[ab_x][ab_y] += 2
+
+
+
+        else:
+
+            t = 5
+            first_data = tab[i][2].split(',')
+            second_data = tab[i][t - 1].split(',')
+
+            if tab[i][1] == "on":
+                change = 1
+            else:
+                change = -1
+
+
+
+
+            for ab_y in range(int(first_data[1]),int(second_data[1]) + 1):
+                for ab_x in range(int(first_data[0]), int(second_data[0]) + 1):
+                    lights[ab_x][ab_y] += change
+                    if lights[ab_x][ab_y] < 0:
+                        lights[ab_x][ab_y] = 0
+
+    return count_lights_p2(lights)
+
+def count_lights_p2(lights):
+    cpt = 0
+    for i in lights:
+        for x in i:
+            cpt += x
+    return cpt
+
+
+print(ex6part2())
