c5bef150d3a853d26ee7d3a4f523d8bb77896f13
Redak92 <bastienguibert98@gmail.com>
2023-09-25 17:31:34 +0200

    y

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-def NOT_gate(input_value):
-    # Utilisez l'opérateur de complément binaire (~) pour inverser les bits
-    result = ~input_value & 0xFFFF  # Assurez-vous que le résultat est sur 16 bits (0xFFFF)
-    return result
+import functions.functions
 
-# Exemple d'utilisation de la porte NOT
-input_value = 12  # Par exemple, un nombre binaire représenté par 12 en décimal
-output_value = NOT_gate(input_value)
-print("Entrée:", input_value)
-print("Sortie:", output_value)
+tab = functions.functions.clear_input("input.txt")
+
+print(tab)
