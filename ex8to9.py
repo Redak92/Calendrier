52b53a577bc9f47a6276affa8dc2783dffdc0073
Redak92 <bastienguibert98@gmail.com>
2023-09-26 16:52:30 +0200

    p

--- ex8to9.py
+++ ex8to9.py
@@ -1,2 +1,16 @@
 from functions import functions
+def ex8():
 
+    with open('input.txt', 'r') as fichier:
+        tab = [eval(line) for line in fichier]
+
+    all_chars = functions.clear_input("input.txt")
+    print(f'sum : {sum([len(line) for line in all_chars]) - sum([len(line) for line in tab])}')
+
+def ex8p2():
+    all_chars = '""'
+
+    print(all_chars)
+
+
+ex8p2()
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import functions.functions
+def encoder_chaine(chaine):
+    # Utilise replace() pour ajouter des caractères d'échappement aux guillemets doubles
+    chaine_encodee = chaine.replace('"', '\\"')
+    return chaine_encodee
 
-tab = functions.functions.clear_input("input.txt")
-
-print(tab)
+# Exemple d'utilisation de la fonction
+chaine_entree = '"'
+chaine_encodee = encoder_chaine(chaine_entree)
+print(chaine_encodee)  # Cela affichera '\"' en sortie
